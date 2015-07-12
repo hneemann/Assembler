8fe4d731559879970862d821b27aaf4bf8c64ce1
hneemann <helmut.neemann@arcor.de>
2015-07-12 18:25:23 +0200

    hexout modified

--- src/main/asm/hexout.asm
+++ src/main/asm/hexout.asm
@@ -1,9 +1,8 @@
 	.const TERMINAL_PORT 0x1f
 
-	ldi r0, 0x1Ab5
-	_call hexOutR0
-	jmp _ADDR_
-
+	LDI R0, 0x1Ab5
+	RCALL RA, hexOutR0
+	BRK
 
 ; write R0 to console as 4 digit hex number
 	.reg DATA r0  ; data
@@ -11,26 +10,26 @@
 	.reg CREG r2  ; return adress register
 
 hexOutR0: 
-	swap DATA
-	swapn DATA
+	SWAP DATA
+	SWAPN DATA
 	RCALL CREG,hexDigitOutR0
-	swapn DATA
+	SWAPN DATA
 	RCALL CREG,hexDigitOutR0
-	swap DATA
-	swapn DATA
+	SWAP DATA
+	SWAPN DATA
 	RCALL CREG,hexDigitOutR0
-	swapn DATA
+	SWAPN DATA
 	RCALL CREG,hexDigitOutR0
-	ret
+	RRET RA
 
 ; write R0 to console as 1 digit hex number
 hexDigitOutR0: 
-	mov DIGIT,DATA
-	andi DIGIT,0xf
-	cpi DIGIT,10
-	brnc h3      ; larger then 10
-	addi DIGIT,'0'
-	jmp h4
-h3:	addi DIGIT,'A'-10
-h4:	out DIGIT,TERMINAL_PORT
-	rret CREG
+	MOV DIGIT,DATA
+	ANDI DIGIT,0xf
+	CPI DIGIT,10
+	BRNC h3      ; larger then 10
+	ADDI DIGIT,'0'
+	JMP h4
+h3:	ADDI DIGIT,'A'-10
+h4:	OUT DIGIT,TERMINAL_PORT
+	RRET CREG
