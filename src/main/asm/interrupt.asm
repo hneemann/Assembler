b363a15a9ea68da7145f86fef697ad0d43d0f765
hneemann <helmut.neemann@arcor.de>
2016-12-21 21:54:33 +0100

    added a RETI instruction, fixed example

--- src/main/asm/interrupt.asm
+++ src/main/asm/interrupt.asm
@@ -53,8 +53,6 @@ h4:     OUT TERMINAL_PORT,DIGIT ; output
 ;********************************************************
 ; ISR to move the LEDs up and down
 ; State machine that returns after LEDs are modified!
-; Register R12 is reserved for use in ISR! 
-; Don't use R12 in main program!
 ;********************************************************
 
 .org 0x1000
@@ -62,36 +60,39 @@ h4:     OUT TERMINAL_PORT,DIGIT ; output
 	.word pos 		; leds position as a bit mask
 	.word dir		; zero means up
 
-	IN R12,0		; read flags
-	PUSH r12		; store flags on stack
-	PUSH r0			; save R0
+	STD [SP-1], R0		; store R0 on stack
+	IN R0, 0		; read the flags
+	SUBI SP,1		; correct stack pointer
+	PUSH R0			; store the flags on stack
+	PUSH R1			; store R1 on stack
 
 	BRK
 
 	LDS R0,dir		; read led direction
-	LDS R12,pos		; read led pos
-	CPI R12,0		; is pos zero?
+	LDS R1,pos		; read led pos
+	CPI R1,0		; is pos zero?
 	BRNE i1			; no? goto i1 
-	LDI R12,1		; yes? Set pos to 1
+	LDI R1,1		; yes? Set pos to 1
 	LDI R0,0		; and direction up
-i1:	OUT LED_PORT, r12	; set leds
+i1:	OUT LED_PORT, R1	; set leds
 	CPI R0,0		; up?
 	BRNE i2			; goto down
-	LSL R12			; shift pos up
-	CPI R12,0x8000		; end reached
+	LSL R1			; shift pos up
+	CPI R1,0x8000		; end reached
 	BRNE i3			; no
 	LDI R0,1		; yes
 	STS dir,R0		; dir to one or down
 	JMP i3			; done
-i2:	LSR R12			; handle down
-	CPI R12,1		; bottom reached
+i2:	LSR R1			; handle down
+	CPI R1,1		; bottom reached
 	BRNE i3			; no? done
 	LDI R0,0		; yes
 	STS dir, R0		; dir to zero or up
-i3:	STS pos,R12		; store pos for next irq
+i3:	STS pos,R1		; store pos for next irq
 
-	POP R0			; resore R0
-	POP R12			; get flags from stack
-	OUT 0, R12		; restore flags
-	IN R12, 1		; get ret addr from IC
-	RRET R12		; return
+	POP R1			; restore R1
+	LD R0,[SP]		; get flags from stack
+	ADDI SP,2		; correct stack pointer
+	OUT 0, R0		; restore flags
+	LDD R0,[sp-1]		; restore R0
+	RETI			; return from ISR
